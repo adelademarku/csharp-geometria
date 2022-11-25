f259d02c07a4a2857c246ea42de99786f228364e
adelademarku <adelademarku@email.com>
2022-11-25 15:23:52 +0100

    Aggiungere i file di progetto.

---
+++ Geometria/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    //definisco la mia classe rettangoli
+    public class Rettangolo
+
+    {
+        //// CARATTERISTICHE / PROPRIETA' che voglio tracciare (di un rettangolo)
+
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+        public int perimetroRettangolo;
+        public int areaRettangolo;
+
+        ////Metodi per calcolare perimetro e area 
+
+        public int CalcolaPerimetro()
+        {
+            return (baseRettangolo + altezzaRettangolo)*2;
+        }
+
+
+        public int CalcolaArea()
+        {
+            return baseRettangolo * altezzaRettangolo;
+        }
+
+        //Metodo per stampare un rettangolo
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("Base:\t" + baseRettangolo + " cm");
+            Console.WriteLine("Altezza:\t" + altezzaRettangolo + " cm");
+            Console.WriteLine("Perimetro:\t" + perimetroRettangolo + " cm");
+            Console.WriteLine("Area:\t" + areaRettangolo + " cm2");
+
+        }
+
+    }
+}
---
+++ Geometria/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Nel progetto csharp-geometria creare le seguenti classi :
+Creare una classe Rettangolo con due attributi interi: baseRettangolo e altezzaRettangolo. 
+Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+Poi, nel vostro programma principale (Program.cs) chiedete all’utente di inserire, da console,
+i valori di base e di altezza con cui istanziare un nuovo Rettangolo. 
+Dopo averlo istanziato, occupatevi di configurare le sue proprietà di base e altezza 
+(come abbiamo fatto con le nostre auto questa mattina nel programma principale) e stampate a video il perimetro e l’area.
+Provate ad istanziare tanti rettangoli con nome diverso e attributi diversi (baseRettangolo e altezzaRettangolo) e
+provate a stampare le loro proprietà ad esempio con un metodo StampaRettangolo che vi stampi il vostro rettangolo ad esempio cosi:
+—— Rettangolo1 ——
+base: 20 cm
+altezza: 10 cm
+Perimetro: 60 cm
+Area: 200 cm2 */
+
+using Geometria;
+
+Rettangolo primoRettangolo = new Rettangolo();
+
+//Chiedo le informazioni al utente 
+Console.WriteLine("inserisci base del rettangolo");
+
+primoRettangolo.baseRettangolo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inserisci altezza del rettangolo");
+
+primoRettangolo.altezzaRettangolo = Convert.ToInt32(Console.ReadLine());
+
+primoRettangolo.perimetroRettangolo = primoRettangolo.CalcolaPerimetro();
+primoRettangolo.areaRettangolo = primoRettangolo.CalcolaArea();
+
+//Stampo tutte le informazioni del primo rettangolo
+
+Console.WriteLine("Vediamo le informazione del primo rettangolo: ");
+primoRettangolo.StampaRettangolo();
