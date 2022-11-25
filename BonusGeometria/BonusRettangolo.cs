6e7cb82eb4f595a89f01174df6ffa8837a429ce6
adelademarku <adelademarku@email.com>
2022-11-25 16:44:15 +0100

    prova bonus

---
+++ BonusGeometria/BonusRettangolo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusGeometria
+{
+    public class BonusRettangolo
+    {
+        //// CARATTERISTICHE / PROPRIETA' che voglio tracciare (di un rettangolo)
+
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+        public int perimetroRettangolo;
+        public int areaRettangolo;
+
+        ////Metodi per calcolare perimetro e area 
+
+        public int CalcolaPerimetro()
+        {
+            return (baseRettangolo + altezzaRettangolo) * 2;
+        }
+
+
+        public int CalcolaArea()
+        {
+            return baseRettangolo * altezzaRettangolo;
+        }
+
+        //Metodo per stampare un rettangolo
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("Base:\t" + baseRettangolo + " cm");
+            Console.WriteLine("Altezza:\t" + altezzaRettangolo + " cm");
+            Console.WriteLine("Perimetro:\t" + perimetroRettangolo + " cm");
+            Console.WriteLine("Area:\t" + areaRettangolo + " cm2");
+
+        }
+
+        //Metodo per stampare la figura del rettangolo 
+      
+        public void StampaDisegnoRettangolo()
+        {
+            Console.Write(" ");
+
+            for (int i = 1; i <= baseRettangolo; i++)
+            {
+                for (int j = 1; j <= altezzaRettangolo; j++)
+                {
+                    if (i == 1 || i == baseRettangolo )
+                    {
+                        Console.Write("-");
+                    } else if (j== 1 || j == altezzaRettangolo)
+                    { Console.WriteLine("|"); }
+                    else 
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.Write("\n ");
+            }
+        }
+    }
+
+}
+
+
+
+
+
+    
---
+++ BonusGeometria/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BonusGeometria;
+
+
+BonusRettangolo primoRettangolo = new BonusRettangolo();
+
+///Chiedo le informazioni al utente 
+Console.WriteLine("inserisci base del rettangolo");
+
+primoRettangolo.baseRettangolo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inserisci altezza del rettangolo");
+
+primoRettangolo.altezzaRettangolo = Convert.ToInt32(Console.ReadLine());
+
+primoRettangolo.perimetroRettangolo = primoRettangolo.CalcolaPerimetro();
+primoRettangolo.areaRettangolo = primoRettangolo.CalcolaArea();
+
+//Stampo tutte le informazioni del primo rettangolo
+
+Console.WriteLine("Vediamo le informazione del primo rettangolo: ");
+primoRettangolo.StampaRettangolo();
+Console.WriteLine("Stampo il disegno");
+primoRettangolo.StampaDisegnoRettangolo();
